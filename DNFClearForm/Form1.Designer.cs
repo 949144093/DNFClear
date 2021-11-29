65805eb2add7bc1026c6f2442c43b41f89f1f9ec
949144093 <949144093@qq.com>
2021-11-29 14:49:38 +0800

    更新2.0版本 深度清理缓存

--- DNFClearForm/Form1.Designer.cs
+++ DNFClearForm/Form1.Designer.cs
@@ -30,54 +30,48 @@ namespace DNFClearForm
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(54, 75);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(496, 23);
-            this.progressBar1.TabIndex = 0;
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(54, 115);
+            this.textBox1.Location = new System.Drawing.Point(54, 83);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(496, 117);
+            this.textBox1.Size = new System.Drawing.Size(496, 149);
             this.textBox1.TabIndex = 1;
             // 
-            // label1
+            // textBox2
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(229, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "正在清理trc文件";
+            this.textBox2.Location = new System.Drawing.Point(54, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(346, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "重新选择DNF安装目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 255);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.progressBar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "DNF缓存文件清理器（作者：502）";
@@ -88,10 +82,9 @@ namespace DNFClearForm
         }
 
         #endregion
-
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DNFClearForm/Form1.cs
+++ DNFClearForm/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace DNFClearForm
 {
@@ -13,38 +14,185 @@ namespace DNFClearForm
         }
         private static readonly string ApplicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
+
+
+        #region 直接删除指定目录下的所有文件及文件夹(保留目录)
+        /// <summary>
+        /// 直接删除指定目录下的所有文件及文件夹(保留目录)
+        /// </summary>
+        /// <param name="strPath">文件夹路径</param>
+        /// <returns>执行结果</returns>
+        public bool DeleteDir(string strPath)
+        {
+            try
+            {
+                // 清除空格
+                strPath = @strPath.Trim().ToString();
+                // 判断文件夹是否存在
+                if (System.IO.Directory.Exists(strPath))
+                {
+                    // 获得文件夹数组
+                    string[] strDirs = System.IO.Directory.GetDirectories(strPath);
+                    // 获得文件数组
+                    string[] strFiles = System.IO.Directory.GetFiles(strPath);
+                    // 遍历所有子文件夹
+                    foreach (string strFile in strFiles)
+                    {
+                        // 删除文件夹
+                        System.IO.File.Delete(strFile);
+                        textBox1.AppendText(strFile+"文件夹已经删除\r\n");
+
+                    }
+                    // 遍历所有文件
+                    foreach (string strdir in strDirs)
+                    {
+                        // 删除文件
+                        System.IO.Directory.Delete(strdir, true);
+                        textBox1.AppendText(strdir + "文件夹已经删除\r\n");
+                    }
+                }
+                // 成功
+                return true;
+            }
+            catch (Exception Exp) // 异常处理
+            {
+                // 异常信息
+                MessageBox.Show("文件占用，请关闭游戏后重试！");
+                // 失败
+                return false;
+            }
+        }
+        public void DeleteDir(string strPath,string strPattern)
+        {
+            try
+            {
+                string[] strFiles = Directory.GetFiles(strPath, strPattern);
+                if (strFiles.Length != 0)
+                {
+                    foreach (string item in strFiles)
+                    {
+                        try
+                        {
+                            File.Delete(item);
+                            textBox1.AppendText(item + "文件已经删除\r\n");
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("文件占用，请关闭游戏后重试！");
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                textBox1.AppendText("当前路径为空，无需清理。\r\n");
+            }
+            
+        }
+        #endregion
+        private void Clear()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (Directory.Exists(config.AppSettings.Settings["folder"].Value))//判断配置的路径是否存在
+            {
+                dialog.SelectedPath = config.AppSettings.Settings["folder"].Value;//若路径存在则自动获取该路径
+                textBox2.Text = dialog.SelectedPath.ToString();
+            }
+            //第一次打开软件需配置DNF安装目录
+            else
+            {
+                if (dialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    string _path = dialog.SelectedPath;//将用户选取的路径值赋值给变量       
+                    config.AppSettings.Settings["folder"].Value = _path; //将用户选取的路径_path赋给app.config中的_path(名称自取)     
+                    config.Save(ConfigurationSaveMode.Modified);       //将配置保存
+                    textBox2.Text = dialog.SelectedPath.ToString();                              //
+                }
+                System.Configuration.ConfigurationManager.RefreshSection("appSettings");//刷新配置文件  
+            }
+
+            string DNFClearPath1 = ApplicationData.Substring(0, ApplicationData.LastIndexOf("\\")) + "\\LocalLow\\DNF";
+            string DNFClearPath2 = ApplicationData.Substring(0, ApplicationData.LastIndexOf("\\")) + "\\Roaming\\Tencent\\dnf_asura\\config";
+            string DNFClearPath3 = ApplicationData.Substring(0, ApplicationData.LastIndexOf("\\")) + "\\Roaming\\Tencent\\Logs";
+            string DNFClearPath4 = ApplicationData.Substring(0, ApplicationData.LastIndexOf("\\")) + "\\Local\\Temp\\qbcore\\cache";
+
+            
+            string DnfSetupPath = textBox2.Text;
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo("C:\\Program Files\\AntiCheatExpert");
+                di.Delete(true);
+                textBox1.AppendText("C:\\Program Files\\AntiCheatExpert文件夹已经删除\r\n");
+            }
+            catch (Exception e)
+            {
+                textBox1.AppendText("C:\\Program Files\\AntiCheatExpert文件夹删除失败或为空，请以管理员身份启动软件！\r\n");
+            }
+
+
+            DeleteDir(DNFClearPath1);
+            DeleteDir(DNFClearPath2);
+            DeleteDir(DNFClearPath3);
+            DeleteDir(DNFClearPath4);
+
+
+            DeleteDir(DnfSetupPath, "*.log");
+            DeleteDir(DnfSetupPath, "*_tmp.dat");
+            DeleteDir(DnfSetupPath, "*_tmp.dat - journal");
+            DeleteDir(DnfSetupPath, "Thread *.z");
+            DeleteDir(DnfSetupPath, "debug.log");
+            DeleteDir(DnfSetupPath, "BugTrace.log");
+            DeleteDir(DnfSetupPath, "file.txt");
+            DeleteDir(DnfSetupPath, "LagLog.txt");
+
+            DeleteDir(DnfSetupPath, "gameloader.log");
+            DeleteDir(DnfSetupPath, "CrashDNF2.cra");
+            DeleteDir(DnfSetupPath, "NetworkDump.dmp");
+            DeleteDir(DnfSetupPath, "vsddrvdll *.org");
+
+            DeleteDir(DnfSetupPath + "\\AntiCheatExpert\\InGame\\x64", "*.rc");
+            DeleteDir(DnfSetupPath + "\\AntiCheatExpert\\InGame\\x64", "*.org");
+            DeleteDir(DnfSetupPath + "\\AntiCheatExpert\\InGame\\x64", "*.bh");
+            DeleteDir(DnfSetupPath + "\\start\\Cross\\Log","*.*");
+
+            DeleteDir(DnfSetupPath + "\\start\\Cross\\Core\\Stable\\log", "tgp_web_log.tlg");
+            DeleteDir(DnfSetupPath + "\\start\\Cross\\Core\\Stable\\Logs", "CrossProxy.tlg.*");
+            DeleteDir(DnfSetupPath + "\\start\\TenProtect\\patchs","*.*");
+
+            DeleteDir(DnfSetupPath + "\\TCLS\\TenProtect", "*.dmp.z");
+            DeleteDir(DnfSetupPath + "\\TCLS\\TenProtect\\TP", "Installer.log");
+            DeleteDir(DnfSetupPath + "\\TCLS\\TenProtect\\TP\\TPHelper", "*.dmp.z");
+            DeleteDir(DnfSetupPath + "\\TCLS\\ALog", "*.log");
+            DeleteDir(DnfSetupPath + "\\TCLS\\tLog", "*.log");
+            DeleteDir(DnfSetupPath + "\\TCLS\\tLog\\Repair", "*.log");
+            DeleteDir(DnfSetupPath + "\\TCLS\\log", "*.*");
+
+
+            textBox1.AppendText("清理完成!\r\n"); 
+        }
+
         private void Form1_Shown(object sender, EventArgs e)
         {
-            string DNFPath = ApplicationData.Substring(0, ApplicationData.LastIndexOf("\\")) + "\\LocalLow\\DNF";
-            string trcPattern = "*.trc";
-            string[] trcFiles = Directory.GetFiles(DNFPath, trcPattern);
-            if (trcFiles.Length == 0)
-            {
-                textBox1.AppendText("当前没有多余trc文件！\r\n");
-                progressBar1.Value = 100;
-                textBox1.AppendText("清理完成!");
-                return;
-            }
-            progressBar1.Value = 0;
-            progressBar1.Maximum = trcFiles.Length;
-            progressBar1.Minimum = 0;
-            foreach (string currentFile in trcFiles)
-            {
-                try
-                {
-                    File.Delete(currentFile);
-                    textBox1.AppendText(currentFile + "文件已经删除\r\n");
-                    progressBar1.Value += 1;
-                    Application.DoEvents();
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("DNF正在运行,进程被占用,请退出游戏后再试！");
-                }
+            Clear();
+        }
 
-            }
-            textBox1.AppendText("清理完成!");
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (dialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                string _path = dialog.SelectedPath;//将用户选取的路径值赋值给变量       
+                config.AppSettings.Settings["folder"].Value = _path; //将用户选取的路径_path赋给app.config中的_path(名称自取)     
+                config.Save(ConfigurationSaveMode.Modified);       //将配置保存
+                textBox2.Text = dialog.SelectedPath.ToString();                              //
+            }
+            System.Configuration.ConfigurationManager.RefreshSection("appSettings");//刷新配置文件 
+
+            Clear();
         }
     }
 }
